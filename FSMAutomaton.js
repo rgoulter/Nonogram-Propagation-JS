b9bf63a1234a01fa78271f80dd1ad984bae33a17
Richard Goulter <richard.goulter@gmail.com>
2012-04-19 20:55:27 +0800

    Added license terms.

--- FSMAutomaton.js
+++ FSMAutomaton.js
@@ -1,3 +1,17 @@
+/*
+
+Copyright (c) 2012, Richard Goulter <richard.goulter@gmail.com> and Ngo Thuy Hang <hangjoni@gmail.com>
+Distributed under the Modified BSD Licence. (http://opensource.org/licenses/BSD-3-Clause).
+
+This NonogramSolver builds upon FD.js by Srikumar K. S. <srikumarks@gmail.com>,
+which can be found on GitHub at https://github.com/srikumarks/FD.js.
+
+The algorithm behind the solver makes use of a regular constraint as described
+in the paper "Regular Language Membership Constraint" by Niko Paltzer.
+http://www.ps.uni-saarland.de/courses/seminar-ws04/papers/paltzer.pdf
+
+*/
+
 var FSM = function(colorsArray) {//colorsArray keeps the colors of the clues (non repetitive)
 	var i, transitions = new Array;
     // transitions is a 2D array. First dimension is input (eg: 0 or 1). Second dimension is 
